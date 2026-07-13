1c916f30cb1fd5b41fc3132195a96e5b2a0bc19f
Mridul <mriduls.brainerhub@gmail.com>
2026-07-13 18:41:17 +0530

    alembic modified

--- alembic/env.py
+++ alembic/env.py
@@ -1,35 +1,64 @@
-import uuid
+from logging.config import fileConfig
 
-from sqlalchemy import Column, String, DateTime, ForeignKey
-from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.sql import func
+from sqlalchemy import engine_from_config, pool
+from alembic import context
 
-from app.db.database import Base
+from app.db.database import DATABASE_URL, Base
+
+# Import ALL your models here
+from app.models.employee import Employee
+from app.models.asset import Asset
+
+config = context.config
+
+config.set_main_option(
+    "sqlalchemy.url",
+    DATABASE_URL,
+)
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
 
 
-class Asset(Base):
-    __tablename__ = "assets"
+def run_migrations_offline() -> None:
+    """Run migrations in offline mode."""
 
-    asset_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    asset_code = Column(String, unique=True, nullable=False)
-    asset_name = Column(String, nullable=False)
-    category = Column(String, nullable=False)
-    brand = Column(String, nullable=False)
-    status = Column(String, nullable=False)
+    url = config.get_main_option("sqlalchemy.url")
 
-    employee_id = Column(
-        UUID(as_uuid=True),
-        ForeignKey("employees.employee_id"),
-        nullable=True,
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
     )
 
-    assigned_at = Column(DateTime(timezone=True), nullable=True)
-    returned_at = Column(DateTime(timezone=True), nullable=True)
+    with context.begin_transaction():
+        context.run_migrations()
 
-    created_at = Column(
-        DateTime(timezone=True),
-        server_default=func.now(),
-        nullable=False,
+
+def run_migrations_online() -> None:
+    """Run migrations in online mode."""
+
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
     )
 
-   
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
