76c4d354e83c462faf6c2aae4ce123c430fef168
Mridul <mriduls.brainerhub@gmail.com>
2026-07-15 14:04:18 +0530

    secret added in .env, .env.example created

--- app/core/auth.py
+++ app/core/auth.py
@@ -14,9 +14,10 @@ from app.models.user import User
 load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(
+    os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+)
 
 pwd_context = CryptContext(
     schemes=["bcrypt"],
